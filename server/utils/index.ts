9f8337b8567c1286ed7236189be6a35487c09670
Matt Powers <powers.matthew.douglas@gmail.com>
2023-07-16 12:38:56 -0700

    testing

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,12 +1,14 @@
 import { pingDB } from '../db';
 import { pingRedis } from '../redis';
 
+//Commit comment
+
 export const initializeApp = async () => {
-	try {
-		await pingDB();
-		await pingRedis();
-		console.log('app initialized');
-	} catch (e) {
-		console.error(e);
-	}
+  try {
+    await pingDB();
+    await pingRedis();
+    console.log('app initialized');
+  } catch (e) {
+    console.error(e);
+  }
 };
